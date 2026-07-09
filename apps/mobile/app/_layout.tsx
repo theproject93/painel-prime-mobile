21406efd37dc7e3774ada0e1b3026906bf9953da
Lucas Ferreira <lucasfer.mail@gmail.com>
2026-07-09 15:47:09 -0300

    feat: add Sentry crash monitoring to mobile app

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,12 +1,23 @@
 import 'react-native-gesture-handler';
 import { Slot } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
 
 import { MobileRootProviders } from '../src/bootstrap/MobileRootProviders';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  _experiments: {
+    profilesSampleRate: 1.0,
+  },
+});
+
+function RootLayout() {
   return (
     <MobileRootProviders>
       <Slot />
     </MobileRootProviders>
   );
 }
+
+export default Sentry.wrap(RootLayout);
